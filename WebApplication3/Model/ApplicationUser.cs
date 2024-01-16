0ea01131fea852f6d09145a6451232f0a84ef8ef
Stried <alanangkh@gmail.com>
2024-01-16 10:04:18 +0800

    Add project files.

---
+++ WebApplication3/Model/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required, DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required, DataType(DataType.Text)]
+        public string CreditCard { get; set; }
+    }
+}
